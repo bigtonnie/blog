8c9781f2693dababe4f4a6906a182cad05f180e9
Anthony-Gicheru <giterugicheru@gmail.com>
2024-10-27 19:05:37 +0300

    Add tests for Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,39 @@
-require "test_helper"
+require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'draft? returns true for draft post' do
+    assert Post.new(published_at: nil).draft?
+  end
+
+  test 'draft? returns false for published post' do
+    refute Post.new(published_at: 1.year.ago).draft?
+  end
+
+  test 'draft? returns false for scheduled post' do
+    refute Post.new(published_at: 1.year.from_now).draft?
+  end
+
+  test 'published? returns true for published post' do
+    assert Post.new(published_at: 1.year.ago).published?
+  end
+
+  test 'published? returns false for draft post' do
+    refute Post.new(published_at: nil).published?
+  end
+
+  test 'published? returns false for scheduled post' do
+    refute Post.new(published_at: 1.year.from_now).published?
+  end
+
+  test 'scheduled? returns true for scheduled post' do
+    assert Post.new(published_at: 1.year.from_now).scheduled?
+  end
+
+  test 'scheduled? returns false for draft post' do
+    refute Post.new(published_at: nil).scheduled?
+  end
+
+  test 'scheduled? returns false for scheduled post' do
+    refute Post.new(published_at: 1.year.ago).scheduled?
+  end
 end
