e4fc1438105f34d9cb961eed00545d83996116c6
Anthony-Gicheru <giterugicheru@gmail.com>
2024-10-29 15:21:18 +0300

    Add cover image upload feature to Post model using ActiveStorage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :published_at)
+    params.require(:post).permit(:title, :content, :cover_image, :published_at)
   end
 
   def set_post
